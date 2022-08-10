c09ab6fc3c7fdad434653b790c45404682eeaf62
Gustavo Almeida <gusalmeida93@gmail.com>
2022-08-10 19:49:03 -0300

    Criado dataframe camada gold e terraform glue crawler

---
+++ terraform/glue.tf
@@ -0,0 +1,14 @@
+resource "aws_glue_catalog_database" "glue_database" {
+    name = "enem-pyspark-athena"
+}
+
+
+resource "aws_glue_crawler" "gold_crawler" {
+    database_name = aws_glue_catalog_database.glue_database.name
+    name          = "enem-pyspark-athena-gold"
+    role          = aws_iam_role.crawler.arn
+
+    s3_target {
+        path = "s3://${aws_s3_bucket.gold.bucket}"
+  }
+}
---
+++ terraform/iam.tf
@@ -0,0 +1,45 @@
+resource "aws_iam_role" "crawler" {
+    name               = "enem-pyspark-athena-glue-crawler"
+    path               = "/mba/xp/"
+    assume_role_policy = data.aws_iam_policy_document.policy_glue_service.json
+}
+
+resource "aws_iam_role_policy" "glue_gold_policy" {
+    name = "enem-pyspark-athena-s3-gold"
+    role = aws_iam_role.crawler.id
+    policy = data.aws_iam_policy_document.policy_gold_bucket.json
+}
+
+data "aws_iam_policy_document" "policy_gold_bucket" {
+
+    statement {
+
+        effect = "Allow"
+
+        actions = [
+
+            "s3:GetObject",
+            "s3:PutObject"
+        ]
+
+        resources = [
+            aws_s3_bucket.gold.arn
+        ]
+    }
+
+}
+
+data "aws_iam_policy_document" "policy_glue_service" {
+
+    statement {
+
+        effect = "Allow"
+        actions = ["sts:AssumeRole"]
+
+        principals {
+            type        = "Service"
+            identifiers = ["glue.amazonaws.com"]
+        }
+    }
+
+}
